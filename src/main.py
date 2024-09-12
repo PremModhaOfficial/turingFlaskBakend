a59e08d855c3da024b088748f675277df8fae34a
Prem Modha <premmodhaofficial@gmail.com>
2024-09-12 17:22:32 +0530

    just do it

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+from turing_machine.turing import TuringMachine
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route("/api/config", methods=["GET"])
+@app.route("/api/config", methods=["GET", "POST"])
 def config():
-    return jsonify({"message": "Hello, World!"})
+    if request.method == "POST":
+        data = request.json
+        print(data)
+        return jsonify(data)
+    return jsonify({})
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
+
+
+def interCepter(config):
+    return TuringMachine(config)
